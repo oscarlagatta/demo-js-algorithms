df7becc14d48b7dcd588a40a7e8336c237b5da0d
Oscar <oscarlagatta@gmail.com>
2024-07-26 11:12:25 +0100

    Add vertices '5' and '6' to the graph
    Added two new vertices to the graph setup. This extends the current graph, allowing for the addition of edges involving these new vertices in the future.

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ myGraph.addVertex('1');
 myGraph.addVertex('2');
 myGraph.addVertex('3');
 myGraph.addVertex('4');
+myGraph.addVertex('5');
+myGraph.addVertex('6');
 myGraph.addEdge('3', '1');
 myGraph.addEdge('3', '4');
 myGraph.addEdge('4', '2');
